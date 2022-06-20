89076d1a4b45f437f1e5ec5288d3147525e3a3ec
kristaver <kristaver@gmail.com>
2022-06-20 14:59:55 +0200

    initialized portfolio page from template

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
+import photo from './pfp.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={photo} className="App-logo" alt="logo" />
+        <h1>
+          this is my portfolio to be
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          the page is under construction
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Kristaver"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          github
         </a>
       </header>
     </div>
